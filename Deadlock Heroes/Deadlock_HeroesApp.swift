cf188828f6763741a0e82cb57922ac5049bfc727
Andrii Pashuta <andrii.pashuta@gmail.com>
2025-01-01 16:05:46 +0100

    Initial Commit

---
+++ Deadlock Heroes/Deadlock_HeroesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Deadlock_HeroesApp.swift
+//  Deadlock Heroes
+//
+//  Created by Andrii Pashuta on 01/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct Deadlock_HeroesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
